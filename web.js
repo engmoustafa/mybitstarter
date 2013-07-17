1354d13028bf7f47d007824f1fbc076c09307f85
engmoustafa <moustafafathi@gmail.com>
2013-07-17 00:36:14 +0000

    edit page

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var http = require('http');
+var fileSystem = require('fs');
+var path = require('path');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('Hello World 2!');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  //res.end('Hello World\n');
+  var filePath = "index.html";
+  var readStream = fileSystem.createReadStream(filePath);  
+  readStream.pipe(response);  
 });
 
 var port = process.env.PORT || 5000;
